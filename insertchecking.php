ad4a689b9af701914b2ff61535e577479fe724bf
Arlene Sagaoinit <asagaoinit1@student.rccd.edu>
2024-04-30 15:07:16 -0700

    added insert checking

---
+++ insertchecking.php
@@ -0,0 +1,36 @@
+<html>
+<body>
+<?php
+
+// Database connection
+$servername = "localhost";
+$username = "quickme1_4211";
+$password = "csci4211";
+$dbname = "dbvpny1qngaxgp";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// get the variables from the URL request string
+$Acct_no = $_REQUEST['Acct_no'];
+$lastname = $_REQUEST['lastname'];
+$firstname = $_REQUEST['firstname'];
+$address = $_REQUEST['address'];
+$email = $_REQUEST['email'];
+$phone = $_REQUEST['phone'];
+$date = $_REQUEST['date'];
+
+$sql = "INSERT INTO checking (Acct_no, lastname, firstname, address, email, phone, date) VALUES ('$Acct_no', '$lastname', '$firstname', '$address', '$email', '$phone', '$date')";
+
+if ($conn->query($sql) === TRUE) {
+   echo "New record created successfully";
+} else {
+   echo "Error: " . $sql . "<br>" . $conn->error;
+}
+   
+$conn->close();
+?>
+</body>
+</html>
